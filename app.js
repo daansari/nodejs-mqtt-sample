494f4c90d993a6187365b175db613193bc8db1e0
Marcus Kida <marcus@kida.io>
2014-11-08 12:46:48 +1100

    Adds event handlers and MQTTS

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://username:password@my.geofancy.com');
 
-client.subscribe('username/hello-world');
-client.publish('username/hello-world', 'hello alltogether!');
+var username = 'YOUR_USERNAME';
+var password = 'YOUR_PASSWORD'
+
+// Crreate MQTT Client, be sure to use mqtts:// to establish an TLS secured connection
+var client = mqtt.connect('mqtts://' + username + ':' + password + '@my.geofancy.com');
+
+// Subscribe to some event handlers
+client.on('connect', function (packet) {
+  console.log('MQTT Client connected.');
+});
+client.on('error', function (error) {
+  console.log('Error when connecting: ', error);
+});
+client.on('disconnect', function() {
+  console.log('MQTT Client disconnected');
+});
+client.on('close', function() {
+  console.log('Connection closed');
+});
 client.on('message', function(topic, message) {
   console.log('Topic: ' + topic + ' Message: ' + message);
 });
+
+// Subscribe to and publish sample topics
+client.subscribe(username + '/hello-world');
+client.publish(username + '/hello-world', 'hello alltogether!');
